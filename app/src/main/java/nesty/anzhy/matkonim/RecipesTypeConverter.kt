0b1835711c3dd6ddae630279f1aef9929b11ba14
nes012 <anzhy.nesty@gmail.com>
2021-09-09 21:14:25 +0300

    adding type converter for our database

---
+++ app/src/main/java/nesty/anzhy/matkonim/RecipesTypeConverter.kt
@@ -0,0 +1,22 @@
+package nesty.anzhy.matkonim
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import nesty.anzhy.matkonim.ui.models.FoodRecipe
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe):String{
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data:String):FoodRecipe{
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+}
