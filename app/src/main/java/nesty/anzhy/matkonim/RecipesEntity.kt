92edcdd13b6767d8d7f92fc0f949fbe85bf0091b
nes012 <anzhy.nesty@gmail.com>
2021-09-09 20:25:59 +0300

    Create Entity for our Room db

---
+++ app/src/main/java/nesty/anzhy/matkonim/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package nesty.anzhy.matkonim
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import nesty.anzhy.matkonim.ui.models.FoodRecipe
+import nesty.anzhy.matkonim.util.Constants.Companion.RECIPES_TABLE
+
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id:Int = 0;
+
+}
