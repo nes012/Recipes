fdfe9a7b0012ec5ecfa19bdc47daa3bb08b9a373
nes012 <anzhy.nesty@gmail.com>
2021-09-12 21:33:45 +0300

    Adding database module

--- app/src/main/java/nesty/anzhy/matkonim/data/LocalDataSource.kt
+++ app/src/main/java/nesty/anzhy/matkonim/data/LocalDataSource.kt
@@ -1,4 +1,14 @@
 package nesty.anzhy.matkonim.data
 
-class LocalDataSource {
+import nesty.anzhy.matkonim.data.database.RecipesDao
+import nesty.anzhy.matkonim.data.database.RecipesEntity
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao
+){
+
+    fun insertRecipes(recipesEntity: RecipesEntity){
+        recipesDao.insertRecipes(recipesEntity)
+    }
 }
