84843d500b0eda2d94f7bde14d9ace63637ded7b
nes012 <anzhy.nesty@gmail.com>
2021-09-22 15:53:46 +0300

    design recipes bottom sheet create fragment extends from bottomSheetFragment

---
+++ app/src/main/java/nesty/anzhy/matkonim/ui/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,22 @@
+package nesty.anzhy.matkonim.ui.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import nesty.anzhy.matkonim.R
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+}
