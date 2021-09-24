1fe7ddda6b1443aee578a873c41a83a3a8f93f8e
nes012 <anzhy.nesty@gmail.com>
2021-09-24 17:55:38 +0300

    Create search recipes API.

--- app/src/main/java/nesty/anzhy/matkonim/data/network/FoodRecipesApi.kt
+++ app/src/main/java/nesty/anzhy/matkonim/data/network/FoodRecipesApi.kt
@@ -11,4 +11,9 @@ interface FoodRecipesApi {
     suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQueryMap: Map<String, String>
+    ): Response<FoodRecipe>
 }
