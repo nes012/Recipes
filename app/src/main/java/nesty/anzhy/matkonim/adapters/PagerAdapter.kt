e0bc93e21b2554ab3f13eeb1a0de7c8fbcac263d
nes012 <anzhy.nesty@gmail.com>
2021-09-25 17:55:40 +0300

    create page adapter class

---
+++ app/src/main/java/nesty/anzhy/matkonim/adapters/PagerAdapter.kt
@@ -0,0 +1,26 @@
+package nesty.anzhy.matkonim.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapter(
+   private val resultBundle: Bundle,
+   private val fragments: ArrayList<Fragment>,
+   private val fragmentActivity: FragmentActivity
+): FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        fragments[position].arguments = resultBundle
+        return fragments[position]
+    }
+    //in this class we will have 3 parameters.
+    //1st is a bundle object. we're going to use bundle to parse the data from details activity to fragments
+    //2nd is list our fragments
+    //3rd - fragment activity
+}
---
+++ app/src/main/java/nesty/anzhy/matkonim/ui/activities/detailactivity/DetailsActivity.kt
@@ -0,0 +1,77 @@
+package nesty.anzhy.matkonim.ui.activities.detailactivity
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TableLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import nesty.anzhy.matkonim.R
+import nesty.anzhy.matkonim.adapters.PagerAdapter
+import nesty.anzhy.matkonim.databinding.ActivityDetailsBinding
+import nesty.anzhy.matkonim.ui.activities.detailactivity.fragments.IngredientFragment
+import nesty.anzhy.matkonim.ui.activities.detailactivity.fragments.InstructionsFragment
+import nesty.anzhy.matkonim.ui.activities.detailactivity.fragments.OverviewFragment
+
+class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar)
+        binding.toolBar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewPager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
