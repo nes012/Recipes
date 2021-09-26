f07d27374121a283b7fa321e85f38edcf3824eb3
nes012 <anzhy.nesty@gmail.com>
2021-09-26 15:43:39 +0300

    modify recipes DiffUtil to generic class

--- app/src/main/java/nesty/anzhy/matkonim/util/RecipesDiffUtil.kt
+++ app/src/main/java/nesty/anzhy/matkonim/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package nesty.anzhy.matkonim.util
 import androidx.recyclerview.widget.DiffUtil
 import nesty.anzhy.matkonim.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ): DiffUtil.Callback() {
     override fun getOldListSize(): Int {
        return oldList.size
