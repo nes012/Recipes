e30d90c45ce8a66333fdfa919994757503f33538
nes012 <anzhy.nesty@gmail.com>
2021-09-26 19:33:46 +0300

    modify Main View Model class. Add functions insert/delete/deleteAll

--- app/src/main/java/nesty/anzhy/matkonim/MainViewModel.kt
+++ app/src/main/java/nesty/anzhy/matkonim/MainViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import nesty.anzhy.matkonim.data.database.entities.FavoritesEntity
 import nesty.anzhy.matkonim.data.database.entities.RecipesEntity
 import nesty.anzhy.matkonim.data.repository.Repository
 import nesty.anzhy.matkonim.models.FoodRecipe
@@ -25,10 +26,29 @@ class MainViewModel @Inject constructor(
 
     /**ROOM DB */
     val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
+    val readFavoriteRecipes: LiveData<List<FavoritesEntity>> = repository.local.readFavoriteRecipes().asLiveData()
+
+
 
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
             repository.local.insertRecipes(recipesEntity)
+        }
+
+    private fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity) =
+        viewModelScope.launch(Dispatchers.IO){
+            repository.local.insertFavoriteRecipes(favoritesEntity)
+        }
+
+    private fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) =
+        viewModelScope.launch(Dispatchers.IO){
+            repository.local.deleteFavoriteRecipe(favoritesEntity)
+        }
+
+
+    private fun deleteAllFavoriteRecipes() =
+        viewModelScope.launch(Dispatchers.IO){
+            repository.local.deleteAllFavoriteRecipes()
         }
 
     /**RETROFIT */
