fb5695c8f63d5caf2a99010e23fcb3a0f9445641
nes012 <anzhy.nesty@gmail.com>
2021-11-04 22:00:29 +0200

    change deprecated method. Check internet connection

--- app/src/main/java/nesty/anzhy/matkonim/util/NetworkListener.kt
+++ app/src/main/java/nesty/anzhy/matkonim/util/NetworkListener.kt
@@ -6,43 +6,37 @@ import android.net.Network
 import android.net.NetworkCapabilities
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class NetworkListener: ConnectivityManager.NetworkCallback() {
+class NetworkListener : ConnectivityManager.NetworkCallback() {
     //we need to override here 2 methods. onAvailable/onLost
-
-
     //default value will be false, but it doesn't even matter
     private val isNetworkAvailable = MutableStateFlow(false)
 
-
     override fun onAvailable(network: Network) {
-       isNetworkAvailable.value = true
+        isNetworkAvailable.value = true
     }
 
     override fun onLost(network: Network) {
         isNetworkAvailable.value = false
     }
 
-
-    fun checkNetworkAvailability(context: Context):MutableStateFlow<Boolean>{
+    fun checkNetworkAvailability(context: Context): MutableStateFlow<Boolean> {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         connectivityManager.registerDefaultNetworkCallback(this)
 
         var isConnected = false
 
-        connectivityManager.allNetworks.forEach{ network->
-
-            //we're going to check each network there is and check if we are online or not
-            val networkCapability = connectivityManager.getNetworkCapabilities(network)
-            networkCapability?.let {
+        //we're going to check each network there is and check if we are online or not
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        capabilities.also {
+            if (it != null) {
                 //here we're checking if our device has internet connection. if it has then we want to set the value to our is connected variable
-                if(it.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)){
+                if (it.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
                     isConnected = true
-                    return@forEach
                 }
             }
         }
-
         isNetworkAvailable.value = isConnected
 
         return isNetworkAvailable
